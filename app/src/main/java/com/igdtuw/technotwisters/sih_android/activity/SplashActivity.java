2945c4c7203ecfc88a9e24835033b27991d31a55
DhritiAggarwal <dhritiaggarwal97@gmail.com>
2017-03-19 15:16:50 +0530

    ui change

--- app/src/main/java/com/igdtuw/technotwisters/sih_android/activity/SplashActivity.java
+++ app/src/main/java/com/igdtuw/technotwisters/sih_android/activity/SplashActivity.java
@@ -43,7 +43,7 @@ public class SplashActivity extends AppCompatActivity implements SharedPreferenc
                     else{
                         // set the new task and clear flags
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        intent.setClass(SplashActivity.this, LoginActivity.class);
+                        intent.setClass(SplashActivity.this, MainActivity.class);
                     }
                     startActivity(intent);
                 }
