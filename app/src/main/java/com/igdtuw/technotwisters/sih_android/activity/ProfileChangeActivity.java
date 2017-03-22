6664535beda632548c22aae71a1064873717ac2b
DhritiAggarwal <dhritiaggarwal97@gmail.com>
2017-03-23 01:13:19 +0530

    ProfileChange Activity

---
+++ app/src/main/java/com/igdtuw/technotwisters/sih_android/activity/ProfileChangeActivity.java
@@ -0,0 +1,20 @@
+package com.igdtuw.technotwisters.sih_android.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.igdtuw.technotwisters.sih_android.R;
+
+/**
+ * Created by Admin on 22-03-2017.
+ */
+
+public class ProfileChangeActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_change);
+    }
+
+}
---
+++ app/src/main/java/com/igdtuw/technotwisters/sih_android/activity/ProfileInfo.java
@@ -0,0 +1,12 @@
+package com.igdtuw.technotwisters.sih_android.activity;
+
+/**
+ * Created by Admin on 23-03-2017.
+ */
+
+public class ProfileInfo {
+    private String name;
+    private String password;
+    private String email;
+
+}
